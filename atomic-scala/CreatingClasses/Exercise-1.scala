9f0ef0a3c4a0a4100591c3ad3a74e2b462cc22af
Juan-Pablo Velez <jpvelez@gmail.com>
2016-05-12 13:15:10 -0400

    Exercise 1 of Creating Classes atom

---
+++ atomic-scala/CreatingClasses/Exercise-1.scala
@@ -0,0 +1,20 @@
+class Giraffe
+class Monkey
+class Lion
+class Tiger
+class Hippo
+
+val g = new Giraffe
+println(g)
+
+val m = new Monkey
+println(m)
+
+val l = new Lion
+println(l)
+
+val t = new Tiger
+println(t)
+
+val h = new Hippo
+println(h)
