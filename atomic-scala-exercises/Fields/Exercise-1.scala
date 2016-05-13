900847c60a5ca8f1ab03a6eb033bf8f244897bc0
Juan-Pablo Velez <jpvelez@gmail.com>
2016-05-13 14:45:36 -0400

    Exercise 1 of Fields atom

---
+++ atomic-scala-exercises/Fields/Exercise-1.scala
@@ -0,0 +1,17 @@
+import com.atomicscala.AtomicTest._
+
+class Cup2 {
+  var percentFull = 0
+  val max = 100
+  def add(increase:Int):Int = {
+    percentFull += increase
+    if (percentFull > max) {
+      percentFull = max
+    }
+    percentFull
+  }
+}
+
+val cup = new Cup2
+cup.add(50) is 50
+cup.add(70) is 100
