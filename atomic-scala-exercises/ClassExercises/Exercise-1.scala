382b1b37bbd17ba0a53fd223fcd06b369a99e07c
Juan-Pablo Velez <jpvelez@spotify.com>
2016-06-21 17:58:41 -0400

    Exercise 1 of Class Exerises atom

---
+++ atomic-scala-exercises/ClassExercises/Exercise-1.scala
@@ -0,0 +1,10 @@
+import com.atomicscala.AtomicTest._
+
+class Dimension(var height: Int, var width: Int)
+
+val c = new Dimension(5, 7)
+c.height is 5
+c.height = 10
+c.height is 10
+c.width = 19
+c.width is 19
